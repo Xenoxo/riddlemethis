b6dfeca83532005ea6d9bf6270af2841e7140c36
Xenoxo <timwyc@gmail.com>
2016-05-18 11:32:11 +0200

    Add meteor.id()  reactive data source to RiddleList so verification of user and author can work on each riddle

--- imports/ui/components/Riddle.jsx
+++ imports/ui/components/Riddle.jsx
@@ -5,7 +5,6 @@ import { Riddles } from '../../api/riddles';
 
 
 export default class Riddle extends Component {
-
 	deleteThisRiddle(){	
 		Meteor.call('riddles.remove', this.props.riddle._id);
 		return console.log("this is the id yo ");
@@ -26,20 +25,22 @@ export default class Riddle extends Component {
 
 				<div className="riddle-content">
 					<h3>
-					{this.props.riddle.riddle}
+						{this.props.riddle.riddle}
 					</h3>
 					<div className="riddle-details">
-					Submitted by {this.props.riddle.username} on {this.props.riddle.submitted.toDateString()}
+						Submitted by {this.props.riddle.username} on {this.props.riddle.submitted.toDateString()}
 					</div>
 				</div>
 
 
 				<div className="solved-spacer">
+				
 				{/*console.log(this.props.currentUser)*/}
+
 					<button className="btn btn-primary">
 						<i className="fa fa-question fa-3x"></i>
 					</button>
-					{ true ? 
+					{ this.props.currentUserId === this.props.riddle.author ? 
 					<div className="delete" onClick={this.deleteThisRiddle.bind(this)}>
 						delete
 					</div> : ''
--- imports/ui/pages/RiddleList.jsx
+++ imports/ui/pages/RiddleList.jsx
@@ -6,21 +6,7 @@ import Riddle from '../components/Riddle.jsx'
 
 
 class RiddleList extends Component {
-	
-	isOwner() {
-		let user = this.props.currentUser._id;
-		let theOwner = this.props.riddles;
 
-		// console.log(this.props.currentUser);
-		// console.log(this.props.currentUser._id);
-		console.log("theUser = " + user);
-		console.log("theOwner = " + theOwner);
-		// console.log("currentUser is " + user + " and riddleOwner is " + theOwner);
-		// console.log(user === theOwner);
-		// return (this.props.currentUser._id === this.props.riddle.author);
-
-		return "yo";
-	}
 
 	onUpVote(){
 		return console.log(this.currentUser._id);
@@ -32,9 +18,7 @@ class RiddleList extends Component {
 				key={riddle._id}
 				riddle={riddle}
 				currentUser={this.props.currentUser}
-				// deleteThisRiddle={() => this.deleteThisRiddle()}
-				isOwner={() => this.isOwner()}
-				// isOwner={this.isOwner()}
+				currentUserId={this.props.currentUserId}
 			/>
 		));
 	}
@@ -53,6 +37,7 @@ export default createContainer(() => {
   return {
   	riddles: Riddles.find({}).fetch(),
     currentUser: Meteor.user(),
+    currentUserId: Meteor.userId(),
   };
 }, RiddleList);
 
