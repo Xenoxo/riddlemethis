a37c26b00e2928c48c7ba48cfd6c60668041a00d
Xenoxo <timwyc@gmail.com>
2016-05-22 18:28:27 +0200

    Build query as custom object so that it can become dynamic going forward allowing search on different riddles

--- imports/api/riddles.js
+++ imports/api/riddles.js
@@ -34,21 +34,56 @@ Meteor.methods({
 			difficulty: 0,
     });
 	},
+
 	'riddles.remove'(riddleId) {
 		check(riddleId, String);
 		Riddles.remove(riddleId);
 	},
+
 	'riddles.upvote'(riddleId, theUser) {
 		check(riddleId, String);
+		check(theUser, String)
+
 		if (! this.userId) {
 			throw new Meteor.Error('must log in to upvote');
 		}
-		//if user has already upvoted, then downvote, otherwise upvote
+		//if [user has already upvoted] 
+		// the current user's listofvoted returns "true" when I plug in (the id of the riddle given) , then decrease riddle's upvote count by 1
+		// otherwise increase upvote by 1 AND add this riddle's id to listofvoted and set to 'true'
+
+		// if(Meteor.users.findOne({_id:theUser}))
+
 		Riddles.update({ _id: riddleId }, { $inc: {upvotes:1} });
+		
+		//
+		//	possibly need to rethink whether I'm going to use arrays vs just a list of {lskajdfslakdjfsdakl: true, kjsdhfsakjhsadjh: false}
+		//	
+		//
+		
+
+
 		// Meteor.users.upsert({listofvoted:{idwouldgohere: 'asdfasfasdfdsafdsa'}});
 	},
-	'listofvoted.update'(userId) {
-		Meteor.users.upsert({_id: userId },{$push : {listofvoted : { riddleid: '12312312312123', voted: false } }});
+
+	'listofvoted.update'(riddleId, user) {		
+
+		// BELOW STATEMENT WORKS GREAT FOR INSERTING
+		// Meteor.users.update({_id: userId._id },{$push:{listofvoted : { riddleid:"kjhdsfkjh13", voted:false, solved:false} }});
+
+		// Work on updating
+		// Meteor.users.update({_id: userId._id },{listofvoted})		
+
+		// Meteor.users.upsert({'_id': userId._id}, {'listofvoted':riddleId} );
+		let ridid = {} 
+		ridid["listofvoted.riddle_id_goeshere.upvoted"] = false;
+		let theuser = Meteor.users.findOne({'_id':user._id});
+		let theuser2 = Meteor.users.findOne(ridid);
+		//let lists = theuser.listofvoted;
+		console.log(theuser2);
+
+		// console.log("the riddleId = "+riddleId+" and userId = "+ userId._id);
+		// console.log(Meteor.users.findOne({_id: userId._id}).listofvoted)
+		// console.log("listofvoted = "+ userId.listofvoted);
 	}
 
 });
--- imports/startup/server/accountconfig.js
+++ imports/startup/server/accountconfig.js
@@ -1,11 +1,24 @@
 
 Accounts.onCreateUser(function(options, user) {
-  user.listofvoted = [];
+  user.listofvoted = {
+  	"riddle_id_goeshere":
+  		{	
+  			"upvoted":false,
+  			"solved":false,
+  		},
+  	
+  	"other_riddle_id":
+  		{	
+  			"upvoted":false,
+  			"solved":false,
+  		},  	
+  };
   return user;
+});
+
   // var d6 = function () { return Math.floor(Random.fraction() * 6) + 1; };
   // user.dexterity = d6() + d6() + d6();
   // // We still want the default hook's 'profile' behavior.
   // if (options.profile)
   //   user.profile = options.profile;
   
-});
