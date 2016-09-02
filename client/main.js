576862843dd9f9e30bbc82a2c4c2a66b08d3d9aa
Xenoxo <timwyc@gmail.com>
2016-09-02 11:35:57 +0200

    Clean up imports/startup/client folder and associated index file to correct loading syntax

--- client/main.js
+++ client/main.js
@@ -1,22 +1,3 @@
-import '../imports/startup/client/routes.jsx';
-import '../imports/startup/accounts-config.js';
-
-
-
-// Old code below - before moving things to new structure
-/*import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
- 
-import App from './App.jsx';
-
-import '../imports/api/riddles.js';
- 
-Meteor.startup(() => {
-  render(<App />, document.getElementById('render-target'));
-}); 
-
-*/
-
-
-
+// import '../imports/startup/client/routes.jsx';
+// import '../imports/startup/accounts-config.js';
+import '../imports/startup/client'
