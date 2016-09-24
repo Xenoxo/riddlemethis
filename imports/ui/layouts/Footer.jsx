2437e905f117647d272f62f92d824632f30d4ebf
Xenoxo <timwyc@gmail.com>
2016-09-24 18:57:44 +0200

    Add links to bottom 3 icons as well as regroup them to appear in the middle

--- imports/ui/layouts/Footer.jsx
+++ imports/ui/layouts/Footer.jsx
@@ -7,17 +7,21 @@ export default class Footer extends Component {
 	render() {
 		return (
 			<div className="footer">
-      Contact me!
-      <div className="footer-container">
-          <div className="contact-points">
-            <i className="fa fa-2x fa-twitter"></i>
-          </div>
-          <div className="contact-points">
-            <i className="fa fa-2x fa-github-alt"></i>
-          </div>
-          <div className="contact-points">
-            <i className="fa fa-2x fa-envelope"></i>
-          </div>
+        
+        <div className="footer-container">
+            <div className="contact-points">
+              
+            </div>
+            <div className="contact-points">
+              
+                <a href="https://twitter.com/besttimyouknow"><i className="fa fa-2x fa-twitter"></i></a>
+                <a href="https://github.com/Xenoxo"><i className="fa fa-2x fa-github-alt"></i></a>
+                <a href="https://github.com/Xenoxo"><i className="fa fa-2x fa-envelope"></i></a>
+              
+            </div>
+            <div className="contact-points">
+              
+            </div>
         </div>
 			</div>
 		);
